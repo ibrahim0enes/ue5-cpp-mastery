761de72cab5f1cf36dda988f2242f69a7f564ce7
İbrahim <ibrahim@example.com>
2026-07-24 05:44:40 +0300

    Add ALock actor with trigger and key item mesh components
    - Implement ALock actor class with root scene component
    - Attach UTriggerComponent for interaction/overlap detection
    - Attach UStaticMeshComponent to represent the key item visually
    - Tag actor as "Lock" for runtime identification

--- 03-dungeon-escape/DungeonEscape/Intermediate/Build/Win64/UnrealEditor/Inc/DungeonEscape/UHT/DungeonEscape.init.gen.cpp
+++ 03-dungeon-escape/DungeonEscape/Intermediate/Build/Win64/UnrealEditor/Inc/DungeonEscape/UHT/DungeonEscape.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeDungeonEscape_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xEA287EE5,
-				0x78670852,
+				0xD4E60CC7,
+				0x26ABBE87,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_DungeonEscape.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_DungeonEscape.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_DungeonEscape(Z_Construct_UPackage__Script_DungeonEscape, TEXT("/Script/DungeonEscape"), Z_Registration_Info_UPackage__Script_DungeonEscape, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xEA287EE5, 0x78670852));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_DungeonEscape(Z_Construct_UPackage__Script_DungeonEscape, TEXT("/Script/DungeonEscape"), Z_Registration_Info_UPackage__Script_DungeonEscape, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xD4E60CC7, 0x26ABBE87));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
